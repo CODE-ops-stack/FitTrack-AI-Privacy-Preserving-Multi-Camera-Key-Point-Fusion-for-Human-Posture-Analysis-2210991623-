f1dc1b0972ed64968227507b74245d797a1cda3d
Harsh_Raj <110251286+CODE-ops-stack@users.noreply.github.com>
2026-04-25 09:18:50 +0530

    Add constants for posture evaluation and weights

---
+++ onstants.py
@@ -0,0 +1,10 @@
+# Some constants used in the project
+MIN_CONFIDENCE_THRESHOLD = 0.2
+FRONT_CAMERA_WEIGHT = 0.6
+SIDE_CAMERA_WEIGHT = 0.4
+GOOD_POSTURE_THRESHOLD = 0.65
+OK_POSTURE_THRESHOLD = 0.45
+# messages
+POSTURE_GOOD = "Posture looks fine"
+POSTURE_OK = "Posture is almost correct"
+POSTURE_BAD = "Posture theek karo"
