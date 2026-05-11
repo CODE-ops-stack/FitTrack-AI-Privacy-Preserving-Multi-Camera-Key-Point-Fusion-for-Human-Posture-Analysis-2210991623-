75207128ae8f6fe0f221d91181aae514c5940b51
Harsh_Raj <harshraj9905406@gmail.com>
2026-05-11 16:25:00 +0530

    Delete feedback_logic.py

--- feedback_logic.py
+++
@@ -1,9 +0,0 @@
-def check_posture(confidence):
-"""Give simple feedback based on confidence"""
-# simple threshold based decision
-if confidence >= 0.65:
-return "Posture looks fine"
-elif confidence >= 0.45:
-return "Posture is almost correct"
-else:
-return "Posture theek karo" # there is risk of injury
